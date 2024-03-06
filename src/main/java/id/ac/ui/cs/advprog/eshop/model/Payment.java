d5d20e5a3f42957bb6687dd1c9c1a8768e2d25c5
prasetyoadii <prasetyo.adi21@ui.ac.id>
2024-03-06 19:08:47 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,12 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 
-@Builder
 @Getter
 public class Payment {
 
@@ -14,15 +13,39 @@ public class Payment {
     String method;
     Order order;
     Map<String, String> paymentData;
-
-    @Setter
     String status;
 
     public Payment(String id, String method, Order order, Map<String, String> paymentData) {
-
+        this(id, method, order, paymentData, "PENDING");
     }
 
     public Payment(String id, String method, Order order, Map<String, String> paymentData, String status) {
-        
+        this.id = id;
+        this.method = method;
+        this.setOrder(order);
+        this.setPaymentData(paymentData);
+        this.setStatus(status);
     }
+
+    protected void setPaymentData(Map<String, String> paymentData) {
+        this.paymentData = null;
+    }
+
+    private void setOrder(Order order) {
+        if (order == null) {
+            throw new IllegalArgumentException("Order cannot be null");
+        }
+        this.order = order;
+    }
+
+    public void setStatus(String status) {
+        List<String> statusList = Arrays.asList("PENDING", "SUCCESS", "REJECTED");
+
+        if (!statusList.contains(status)) {
+            throw new IllegalArgumentException("Invalid payment status");
+        }
+        this.status = status;
+    }
+
+    
 }
